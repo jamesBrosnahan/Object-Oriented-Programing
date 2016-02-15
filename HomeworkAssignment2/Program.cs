1e12a76c0711f7e2f92c4e82f7b635bf0bc3f1b5
James Brosnahan <james.p.brosnahan@outlook.com>
2016-02-14 20:15:03 -0500

    Added classes for homework 2

---
+++ HomeworkAssignment2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Axe = new Axe(2);
+            var Club = new Club(1);
+            var Bow = new Bow(100); 
+
+        }
+    }
+}
---
+++ HomeworkAssignment2/Weapon.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAssignment2
+{
+    public class Weapon
+    {
+        protected Weapon(int speed, int damage)
+        {
+            Speed = speed;
+            Damage = damage;
+        }
+        public int Speed
+        {
+            get;
+            protected set;
+        }
+        public int Damage
+        {
+            get;
+            protected set;
+        }
+    }
+    class Sword : Weapon
+    {
+        public int Handed
+        {
+            get;
+            private set;
+        }
+        public Sword(int handed) 
+            : base(100, 100)
+        {
+            Handed = handed;
+        }
+    }
+    class Axe : Weapon
+    {
+        public int Handed
+        {
+            get;
+            private set;
+        }
+        public Axe(int handed) 
+            : base(50, 150)
+        {
+            Handed = handed;
+        }
+    }
+    class Club : Weapon
+    {
+        public int Handed
+        {
+            get;
+            private set;
+        }
+        public Club(int handed)
+            : base(50, 50)
+        {
+            Handed = handed;
+        }
+    }
+    class Bow : Weapon
+    {
+        private int Handed = 2;
+        public int Range
+        {
+            get;
+            private set;
+        }
+        public Bow(int range)
+            : base(500, 10)
+        {
+            Range = range;
+        }
+    }
+}
